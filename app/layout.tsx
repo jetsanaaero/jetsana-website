af1387381e260c3dfc2331a21c43578162455b55
Shintu Cherian <shintucherian@Shintus-MacBook-Air.local>
2026-03-16 19:25:03 +0530

    added google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -56,6 +56,15 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <head>
+	<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-0TBF8LJ1L5"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-0TBF8LJ1L5');
+</script>
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <script
           type="application/ld+json"
